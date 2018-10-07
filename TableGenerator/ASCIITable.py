b4c133f311441c1727cf59673583dcee4972b61b
michalcza <mczarnecki@gmail.com>
2018-10-07 08:54:51 -0600

    Optimization and functionality
    Adding passing of arguments, checking for arguments passed, oand overall functionality improvements.
    
    Co-Authored-By: burrwebb <burrwebb@users.noreply.github.com>

--- TableGenerator/ASCIITable.py
+++ TableGenerator/ASCIITable.py
@@ -1,24 +1,35 @@
 #!/usr/bin/env python
+# CREATES 80 CHAR WIDE ASCII TABLE FROM CSV FILE WITH NEWLINE AS NEEDED IN EACH ROW
 # -*- coding: utf-8 -*-
-import pandas
-import beautifultable
+# import pandas
+# import beautifultable
+import sys
+import os
+import os.path
 
-#infile = "/home/michal/Documents/Projects/Scripting/TableGenerator/input/Book2.csv"
-infile = "/home/michal/Documents/Projects/Scripting/TableGenerator/input/ANSI.csv"
-my_dataframe = pandas.read_csv(infile, skip_blank_lines=True)
-
-headers = list(my_dataframe)
-lists = my_dataframe.values.tolist()
-my_table = beautifultable.BeautifulTable()
-my_table.column_headers = headers
-
-for item in lists:
-    my_table.append_row(item)
-    
-print(my_table)
-
-#outfile_html = "/home/michal/Documents/Projects/Scripting/TableGenerator/output/Book2.html"
-#outfile = "/home/michal/Documents/Projects/Scripting/TableGenerator/output/Book2.txt"
-#print(my_dataframe)
-#pandas.DataFrame.to_string(outfile)
-#encoding="ISO-8859-1"
+try:
+    sys.argv[1:]
+    # INPUT
+    INFILE = sys.argv[1]
+    # CHECK IF FILE EXISTS
+    if os.path.isfile(INFILE) and os.access(INFILE, os.R_OK):
+        print " **** INPUT FILE EXISTS AND IS READABLE, CONTINUING. ****"
+        my_dataframe = pandas.read_csv(INFILE, skip_blank_lines=True)
+        headers = list(my_dataframe)
+        lists = my_dataframe.values.tolist()
+        my_table = beautifultable.BeautifulTable()
+        my_table.column_headers = headers
+        # WRITE TO TABLE
+        for item in lists:
+            my_table.append_row(item)
+        # OUTPUT RESULTS
+            print(my_table)
+            print >>f1, 'THIS IS WHERE WE CAN PRINT TO A FILE VERSUS BASH REDIRECT'
+            exit(1)
+    else:
+        print "**** INPUT FILE IS MISSING OR NOT READABLE ****"
+        print " Usage syntax is $> python ASCIItable.py {filename}"
+        exit(1)
+except IndexError:
+        print " **** PLEASE SPECIFY FILE TO CONVERT ****"
+        sys.exit()
