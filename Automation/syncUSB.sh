88fd8ef05a3b81a78f96e21270b70ea3bec36ddf
michalcza <mczarnecki@gmail.com>
2018-11-23 22:11:31 -0700

    Verified working version.

--- Automation/syncUSB.sh
+++ Automation/syncUSB.sh
@@ -11,26 +11,35 @@
 ###################################################################
 #To Do          :Execute on mount
 ###################################################################
+#Status         :Development
+###################################################################
 #
 # Set exit code to assume bad exit
 exitcode=1
 # Declare variables
 USB="/Volumes/ESD-USB"
 devices="diskutil list | grep -i external"
-local="~/Desktop/USBSync"
-sync="rsync -avun --inplace --progress --exclude='.Trashes' --exclude='.Spotlight-V100' --exclude'.fseventsd'"
+local="/Desktop/USBSync"
+sync="rsync -a --inplace"
+location="$HOME${local}"
+exclude1=".Spotlight-V100"
+exclude2=".Trashes"
+exclude3=".fseventsd"
 # Test if local and remote folders/devices exist.
-if test -e $mount && test -e $local_folder ;
-    then
+if  test -e $USB && test -e $location
+then
+    echo "Tested for $location, found it."
+    echo "Tested for $USB, found it."
+    echo "Continuing with code 0"
+# Rsync USB>LOCAL and if success, Rsync LOCAL>USB
+    $sync $location $USB && $sync $USB $location && echo "Rsync between $USB and $location has completed."
     exitcode=0
-    # Rsync USB>LOCAL and if success, Rsync LOCAL>USB
-    $sync $local $USB && $sync $USB $local
-    echo "Rsync between $mount and $local_folder has completed. Some hidden Mac OS index,trash, and notification files were excluded."
-    echo "Exiting with exit code: $exitcode"
+
+else
+    echo "Tested for $location, didn't find it."
+    echo "Tested for $USB, didn't find it"
+    echo "Exiting with code 1"
+    exitcode=1
 fi
-# Exit if things didn't work out.
-exit $exitcode
-echo "$USB not found."
-echo "Looking for: $USB"
-echo "Found $devices"
-echo "Exiting with exit code: $exitcode"
+#
+exitcode=0
