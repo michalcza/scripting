e447504ef8b7179c0b941c9541e4a790a54bad2c
Michal Czarnecki - Win10 <mczarnecki@gmail.com>
2020-10-04 07:31:10 -0600

    Working commit for file sync.

--- TableGenerator/ASCIITable.py
+++ TableGenerator/ASCIITable.py
@@ -5,6 +5,9 @@
 # TODO ADD SEPARATOR SELECTION CHARACTERS INTO GUI
 # TODO ERROR HANDLING
 # TODO OUTPUT AND INPUT DIRECTORIES
+# SOURCES
+# https://pysimplegui.readthedocs.io/en/latest/cookbook/
+# https://www.geeksforgeeks.org/user-input-in-pysimplegui/
 #
 import os
 import os.path
@@ -14,7 +17,35 @@ import pandas
 import PySimpleGUI as sg
 
 if len(sys.argv) == 1:
-    fname = sg.popup_get_file('CSV to open')
+
+    if len(sys.argv) == 1:
+        fname = sg.Window('CSV to ASCII',
+                          [[sg.Text('This will convert CSV to fixed width ASCII text file')],
+                           [sg.Text('_' * 80)],
+                           [sg.Text('Left border character', size=(30, 1)), sg.InputText('|', size=(1, 1))],
+                           [sg.Text('Right border character', size=(30, 1)), sg.InputText('|', size=(1, 1))],
+                           [sg.Text('Top border character', size=(30, 1)), sg.InputText('#', size=(1, 1))],
+                           [sg.Text('Bottom border character', size=(30, 1)), sg.InputText('=', size=(1, 1))],
+                           [sg.Text('Header separator character', size=(30, 1)), sg.InputText('#', size=(1, 1))],
+                           [sg.Text('Row separator character', size=(30, 1)), sg.InputText('-', size=(1, 1))],
+                           [sg.Text('Intersection character', size=(30, 1)), sg.InputText('+', size=(1, 1))],
+                           [sg.Text('Column separator character', size=(30, 1)), sg.InputText(':', size=(1, 1))],
+                           [sg.Text('Table Width', size=(15, 1)),
+                            sg.Spin(values=[i for i in range(1, 500)], initial_value=100, size=(3, 1)),
+                            sg.Text('Cell Padding', size=(18, 1)),
+                            sg.Spin(values=[i for i in range(1, 10)], initial_value=2, size=(2, 1))],
+                           [sg.Text('_' * 80)],
+                           [sg.Text('CSV to open')],
+                           [sg.In(), sg.FileBrowse()],
+                           [sg.Open(), sg.Cancel()]]).read(close=True)[1][0]
+    else:
+        fname = sys.argv[1]
+
+    if not fname:
+        sg.popup("Cancel", "No filename supplied")
+        raise SystemExit("Cancelling: no filename supplied")
+    #else:
+        #sg.popup('The filename you chose was', fname)
 
 else:
     fname = sys.argv[1]
@@ -72,6 +103,7 @@ try:
         #CREATE NEW NAME FOR OUTPUT FILE
         filename = "%s.txt" % fname
         print(filename)
+
         #WRITE TO NEW FILE
         with open(filename, 'w') as f:
             print(my_table, file=f)
